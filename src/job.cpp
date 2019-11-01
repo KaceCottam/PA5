3dd529afc6b79baf3660bb96f9130c9700fcf26b
maia <maiawhitley@outlook.com>
2019-11-01 13:41:52 -0700

    added all getters

--- src/job.cpp
+++ src/job.cpp
@@ -36,4 +36,8 @@ std::ostream &operator<<(std::ostream &stream, const Job &job) {
 // getters
 [[nodiscard]] unsigned int Job::get_id() const noexcept { return id; }
 
-[[nodiscard]] unsigned int Job::get_n_procs() const noexcept {}
+[[nodiscard]] unsigned int Job::get_n_procs() const noexcept { return n_procs; };
+
+[[nodiscard]] unsigned int Job::get_n_ticks() const noexcept { return n_ticks; };
+
+[[nodiscard]] std::string Job::get_desc() const noexcept { return desc; };
