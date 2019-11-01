9de6c83373a65ac2e1a76ef9aa460d18a8b1bed6
Kace Cottam <kace.cottam@wsu.edu>
2019-11-01 14:01:21 -0700

    Finished main

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 /**
  * @brief output to cerr any errors from f
@@ -21,10 +23,29 @@ int main(int argc, char *argv[]) {
   // start a job with 5 processors
   JobScheduler scheduler{5};
 
-  if (argc == 1) {
-    std::cerr << "Usage: PA5 {target file}" << std::endl;
-    return EXIT_FAILURE;
-  }
+  if (argc == 2) {
+    auto file = std::ifstream(argv[1]);
 
-  scheduler.set_target(std::ifstream(argv[1]));
+    scheduler.set_target(file);
+
+    while (scheduler.is_running())
+      log_error([&] { return scheduler.tick(); });
+
+  } else {
+    std::cout << "Enter a list of jobs, ending with EOF, separating with \\n."
+              << std::endl;
+    std::stringstream input{};
+    std::string next_input{};
+    while (true) {
+      std::cin >> next_input;
+      if (next_input == "EOF")
+        break;
+      input << next_input << " ";
+    }
+
+    scheduler.set_target(input);
+
+    while (scheduler.is_running())
+      log_error([&] { return scheduler.tick(); });
+  }
 }
